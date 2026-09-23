049829e4e675c7519e184c4c4c3c5b7c78e99c12
MugeshMS <165276637+MugeshMS@users.noreply.github.com>
2026-09-23 18:16:36 +0530

    Chapter-2
    Removed unnecessary tips for running code.

--- psws_ap_b_ex/src/main/java/org/example/Main.java
+++ psws_ap_b_ex/src/main/java/org/example/Main.java
@@ -6,8 +6,6 @@ import org.example.instance.Parrot;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContextExtensionsKt;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
     var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
